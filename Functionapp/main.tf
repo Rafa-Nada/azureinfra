bddcd7f6159c81d20b640ab1dbf87353abfa33dd
Rafa-Nada <66781988+Rafa-Nada@users.noreply.github.com>
2025-06-11 11:20:57 +0530

    Fixed the deprecated block related details

--- Functionapp/main.tf
+++ Functionapp/main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.60.0"  # or whatever is latest
+      version = ">= 3.60.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
- subscription_id = var.subscription_id
+  subscription_id = var.subscription_id
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -19,13 +19,13 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
+  resource_group_name     = azurerm_resource_group.rg.name
+  location                = azurerm_resource_group.rg.location
+  account_tier            = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_app_service_plan" "plan" {
+resource "azurerm_service_plan" "plan" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -41,15 +41,13 @@ resource "azurerm_function_app" "function" {
   name                       = var.function_app_name
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
-  app_service_plan_id        = azurerm_app_service_plan.plan.id
+  app_service_plan_id        = azurerm_service_plan.plan.id
   storage_account_name       = azurerm_storage_account.sa.name
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
   version                    = "~4"
 
   site_config {
-    application_stack {
-      dotnet_version = "6"  # Change to node_version, python_version, etc. as needed
-    }
+    dotnet_framework_version = "v6.0"
   }
 
   app_settings = {
